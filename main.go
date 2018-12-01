4cfecb1fd4bc8ed7189ebdf34c6c6f7fdfcf9aac
Ivan Bragin <aywan@aywan.ru>
2018-12-01 16:17:42 +0700

    execution worker

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 var (
@@ -22,6 +25,42 @@ func main() {
 	log.Printf("inflignt: %d\n", *inflight)
 	log.Printf("command: %s %s\n", binnary, strings.Join(arguments, " "))
 
+	wg := &sync.WaitGroup{}
+	workerCh := make(chan string, *inflight)
+	for i := 0; i < *inflight; i++ {
+		wg.Add(1)
+		go execWorker(workerCh, wg)
+	}
+
+	checkList := make([]string, 60)
+	for i := 0; i < 60; i++ {
+		checkList[i] = fmt.Sprintf("%d", i+1)
+	}
+
+	for _, arg := range checkList {
+		workerCh <- arg
+	}
+	close(workerCh)
+	wg.Wait()
+}
+
+func execWorker(ch chan string, wg *sync.WaitGroup) {
+	args := make([]string, len(arguments))
+	copy(args, arguments)
+	for arg := range ch {
+		if commandArgId >= 0 {
+			args[commandArgId] = arg
+		}
+		cmd := exec.Command(binnary, args...)
+		cmd.Env = os.Environ()
+
+		if b, err := cmd.Output(); err != nil {
+			log.Println(err)
+		} else {
+			fmt.Print(string(b))
+		}
+	}
+	wg.Done()
 }
 
 func loadFlags() {
