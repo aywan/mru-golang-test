8ff9dec61d43c3103657a4a914fbdbc3d6cf5e3c
Ivan Bragin <aywan@aywan.ru>
2018-12-01 14:37:49 +0700

    command args parsing

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+var (
+	rate         *int = flag.Int("rate", 0, "rate limit of execution")
+	inflight     *int = flag.Int("inflight", 1, "count of parallel execution")
+	arguments    []string
+	commandArgId int = -1
+	binnary      string
+)
 
 func main() {
-	fmt.Println("Hello mail.ru!")
+	loadFlags()
+
+	log.Printf("rate: %d\n", *rate)
+	log.Printf("inflignt: %d\n", *inflight)
+	log.Printf("command: %s %s\n", binnary, strings.Join(arguments, " "))
+
+}
+
+func loadFlags() {
+	flag.Parse()
+	arguments = flag.Args()
+	if len(arguments) < 1 {
+		log.Fatalf("No arguments supply")
+	}
+	var err error
+	binnary, err = exec.LookPath(arguments[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	arguments = arguments[1:]
+	for i, s := range arguments {
+		if s == "{}" {
+			commandArgId = i
+			break
+		}
+	}
 }
